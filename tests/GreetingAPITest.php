4d19e9a7b307bb96bee99aeda52b6ec16b0eef68
Rocco Palladino <rpalladino@gmail.com>
2018-09-16 15:23:26 -0500

    Expect that a new greeting job be dispatched

--- tests/GreetingAPITest.php
+++ tests/GreetingAPITest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Response;
+use App\Jobs\GreetingJob;
 
 class GreetingAPITest extends TestCase
 {
@@ -9,8 +10,10 @@ class GreetingAPITest extends TestCase
      * @test
      * @return void
      */
-    public function shouldAcceptNewGreeting()
+    public function postNewGreetingShouldRespondWithAccepted()
     {
+        $this->expectsJobs(GreetingJob::class);
+
         $this->post('/greetings/alexa');
 
         assertThat($this->response->status(), is(Response::HTTP_ACCEPTED));
