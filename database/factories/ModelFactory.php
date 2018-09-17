4acf0179cf06e6d91687c4c8b89581d599890a82
Rocco Palladino <rpalladino@gmail.com>
2018-09-16 22:53:24 -0500

    Verify with test that we can list all greetings

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,9 +11,8 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Greeting::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->email,
     ];
 });
--- tests/GreetingAPITest.php
+++ tests/GreetingAPITest.php
@@ -1,10 +1,16 @@
 <?php
 
-use Illuminate\Http\Response;
+use App\Greeting;
 use App\Jobs\GreetingJob;
+use Illuminate\Http\Response;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class GreetingAPITest extends TestCase
 {
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
     /**
      *
      * @test
@@ -17,5 +23,19 @@ class GreetingAPITest extends TestCase
         $this->post('/greetings/alexa');
 
         assertThat($this->response->status(), is(Response::HTTP_ACCEPTED));
+    }
+
+    /**
+     * @test
+     */
+    public function getNamesThatHaveBeenGreeted()
+    {
+        $alexa = factory(Greeting::class)->create(['name' => 'Alexa']);
+        $brian = factory(Greeting::class)->create(['name' => 'Brian']);
+
+        $this->get('/greetings');
+
+        assertThat($this->response->content(), containsString('Alexa'));
+        assertThat($this->response->content(), containsString('Brian'));
     }
 }
